1e33b9b0a553c452950c35c5996b168f9ecb50c5
Alex H <48185123+Vodes@users.noreply.github.com>
2023-12-11 14:03:20 +0100

    Update ktor and add scrimage deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val vaadinVersion: String by extra
 val karibuDslVersion: String by extra
 val ktorVersion: String by extra
+val scrimageVersion: String by extra
 
 plugins {
     kotlin("jvm") version "1.9.21"
@@ -20,6 +21,7 @@ repositories {
     mavenCentral()
     mavenLocal()
     maven("https://jitpack.io")
+    maven("https://maven.vaadin.com/vaadin-addons")
 }
 
 tasks.withType<Test> {
@@ -30,9 +32,13 @@ tasks.withType<Test> {
 }
 
 dependencies {
+    // Kotlin Stuff
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.21")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
 
+    // Vaadin Stuff
     implementation("com.vaadin:vaadin-core:$vaadinVersion") {
         afterEvaluate {
             if (vaadin.productionMode) {
@@ -43,21 +49,22 @@ dependencies {
     implementation("com.github.mvysny.karibudsl:karibu-dsl-v23:$karibuDslVersion")
     implementation("com.github.mvysny.vaadin-boot:vaadin-boot:12.1")
     implementation("org.parttio:line-awesome:2.0.0")
-    //implementation("org.vaadin:spinkit:3.0.0")
-    //implementation("org.vaadin.filesystemdataprovider:filesystemdataprovider:4.0.0")
-    // https://mvnrepository.com/artifact/org.vaadin.filesystemdataprovider/filesystemdataprovider
-    //implementation("org.vaadin.filesystemdataprovider:filesystemdataprovider:4.0.0")
+    implementation("org.vaadin.filesystemdataprovider:filesystemdataprovider:4.0.0")
 
-
-    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    // Misc
     implementation("com.mysql:mysql-connector-j:8.2.0")
     implementation("org.slf4j:slf4j-simple:2.0.7")
-
-    implementation("moe.styx:styx-db:0.2")
     implementation("net.peanuuutz.tomlkt:tomlkt:0.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
 
+    // Custom
+    implementation("moe.styx:styx-db:0.2")
+
+    // Image Processing
+    implementation("com.sksamuel.scrimage:scrimage-core:$scrimageVersion")
+    implementation("com.sksamuel.scrimage:scrimage-webp:$scrimageVersion")
+    implementation("com.sksamuel.scrimage:scrimage-formats-extra:$scrimageVersion")
+
+    // Ktor
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-java:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
