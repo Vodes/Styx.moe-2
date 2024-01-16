e0f68bebbb4eea718f9e736555166ae40a1840a3
Alex H <48185123+Vodes@users.noreply.github.com>
2024-01-16 23:02:46 +0100

    Add checkbox to make a "media" a movie

--- src/main/kotlin/moe/styx/web/components/media/MediaOverview.kt
+++ src/main/kotlin/moe/styx/web/components/media/MediaOverview.kt
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.tabs.Tab
 import com.vaadin.flow.theme.lumo.LumoUtility.Padding
@@ -16,6 +17,8 @@ import moe.styx.db.StyxDBClient
 import moe.styx.db.getMedia
 import moe.styx.db.save
 import moe.styx.types.Media
+import moe.styx.types.toBoolean
+import moe.styx.types.toInt
 import moe.styx.web.data.getAniListDataForID
 import moe.styx.web.getDBClient
 import moe.styx.web.getFirstIDFromMap
@@ -32,24 +35,34 @@ class MediaOverview(media: Media?) : KComposite() {
     val root = ui {
         verticalLayout {
             h2(if (media == null) "Creating new Media" else "Editing ${media.name}")
-            button("Fill from AniList") {
-                onLeftClick {
-                    val id = internalMedia.getFirstIDFromMap(StackType.ANILIST)
-                    if (id == null)
-                        Notification.show("No AniList ID was found in the mapping.").also { return@onLeftClick }
+            horizontalLayout(false) {
+                defaultVerticalComponentAlignment = FlexComponent.Alignment.CENTER
+                button("Fill from AniList") {
+                    onLeftClick {
+                        val id = internalMedia.getFirstIDFromMap(StackType.ANILIST)
+                        if (id == null)
+                            Notification.show("No AniList ID was found in the mapping.").also { return@onLeftClick }
 
-                    val result = getAniListDataForID(id!!)
-                    if (result == null)
-                        Notification.show("Could not get data from AniList API.").also { return@onLeftClick }
+                        val result = getAniListDataForID(id!!)
+                        if (result == null)
+                            Notification.show("Could not get data from AniList API.").also { return@onLeftClick }
 
-                    internalMedia = internalMedia.copy(
-                        nameJP = result!!.title.romaji,
-                        nameEN = result.title.english,
-                        synopsisEN = result.description,
-                        genres = result.genres.joinToString(", "),
-                        tags = result.tags.filter { it.rank > 60 && !it.isMediaSpoiler }.take(10).joinToString(", ") { it.name }
-                    )
-                    updateTabs()
+                        internalMedia = internalMedia.copy(
+                            nameJP = result!!.title.romaji,
+                            nameEN = result.title.english,
+                            synopsisEN = result.description,
+                            genres = result.genres.joinToString(", "),
+                            tags = result.tags.filter { it.rank > 60 && !it.isMediaSpoiler }.take(10).joinToString(", ") { it.name }
+                        )
+                        updateTabs()
+                    }
+                }
+                checkBox("Movie") {
+                    value = !internalMedia.isSeries.toBoolean()
+                    addValueChangeListener {
+                        internalMedia = internalMedia.copy(isSeries = (!it.value).toInt())
+                        updateTabs()
+                    }
                 }
             }
             tabSheet {
