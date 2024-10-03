539850f7d8cc0b1e71a70bd3e05bd4dead18da3e
Alex H. <48185123+Vodes@users.noreply.github.com>
2024-10-03 15:25:52 +0200

    Bump deps and add new templates

--- src/main/kotlin/moe/styx/web/components/Templates.kt
+++ src/main/kotlin/moe/styx/web/components/Templates.kt
@@ -4,15 +4,19 @@ import com.github.mvysny.karibudsl.v10.contextMenu
 import com.github.mvysny.karibudsl.v10.item
 import com.vaadin.flow.component.textfield.TextField
 import moe.styx.common.data.Media
+import moe.styx.downloader.downloaderConfig
+import moe.styx.downloader.utils.removeKeysFromURL
+import moe.styx.web.topNotification
 
 fun TextField.addRegexTemplateMenu(m: Media, rss: Boolean = false) {
     val field = this@addRegexTemplateMenu
-    val jpName = resc(m.nameJP ?: m.name)
-    val enName = resc(m.nameEN ?: m.name)
+    val jpName = resc((m.nameJP ?: m.name).replace(":", " "))
+    val enName = resc((m.nameEN ?: m.name).replace(":", " "))
     val ext = if (rss) "" else "\\.mkv"
     contextMenu {
         target = this@addRegexTemplateMenu
         item("SubsPlease", { field.value = "\\[SubsPlease\\].*($jpName).*\\(1080p\\).*\\.mkv" })
+        item("Erai-Raws", { field.value = "\\[Erai-Raws\\].*($jpName).*\\[1080p\\].*\\.mkv" })
         item("SubsPlus+", { field.value = "\\[SubsPlus\\+\\] ($enName) - S\\d+E\\d+.*WEB.*1080p.*ADN.*$ext" })
         item("GerFTP CR Sub", { field.value = "($jpName) E\\d+.* \\[1080p\\]\\[AAC\\]\\[JapDub\\]\\[GerEngSub\\].*\\.mkv" })
         item("GerFTP CR Dub", { field.value = "($jpName) E\\d+.* \\[1080p\\]\\[AAC\\]\\[GerJapDub\\]\\[GerEngSub\\].*\\.mkv" })
@@ -29,6 +33,14 @@ fun TextField.addRSSTemplateMenu() {
         target = this@addRSSTemplateMenu
         item("Nyaa RSS", { field.value = "https://nyaa.si/?page=rss&c=1_2&f=1&q=" })
         item("Tosho RSS", { field.value = "https://feed.animetosho.org/rss2?q=" })
+        if (downloaderConfig.rssConfig.feedTemplates.isNotEmpty()) {
+            downloaderConfig.rssConfig.feedTemplates.forEach { (name, url) ->
+                item(name, {
+                    field.value = "%$name%"
+                    topNotification("Template goes to: ${removeKeysFromURL(url)}")
+                })
+            }
+        }
     }
 }
 
