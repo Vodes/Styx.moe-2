7679d46d8804e73619922a4a56027a9cea0fe90c
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-01-16 22:45:50 +0100

    Add first time admin setup process

--- src/main/kotlin/moe/styx/web/views/AdminView.kt
+++ src/main/kotlin/moe/styx/web/views/AdminView.kt
@@ -6,16 +6,17 @@ import com.vaadin.flow.component.UI
 import com.vaadin.flow.router.*
 import com.vaadin.flow.server.VaadinRequest
 import com.vaadin.flow.theme.lumo.LumoUtility
-import moe.styx.web.checkAuth
+import moe.styx.common.data.User
+import moe.styx.db.tables.UserTable
+import moe.styx.web.*
 import moe.styx.web.components.MediaGrid
 import moe.styx.web.components.authProgress
 import moe.styx.web.components.misc.categoryListing
 import moe.styx.web.components.misc.generateUnwatched
 import moe.styx.web.components.noAccess
 import moe.styx.web.components.user.userListing
-import moe.styx.web.createComponent
 import moe.styx.web.layout.MainLayout
-import moe.styx.web.topNotification
+import org.jetbrains.exposed.sql.selectAll
 
 @Route("admin", layout = MainLayout::class)
 @PageTitle("Styx - Admin")
@@ -27,7 +28,43 @@ class AdminView : KComposite(), HasUrlParameter<String> {
             setClassNames2(LumoUtility.Margin.NONE, LumoUtility.Padding.NONE)
             authProgress()
         }.also { layout ->
-            checkAuth(UI.getCurrent(), VaadinRequest.getCurrent(), 50, layout, notLoggedIn = { noAccess() }) {
+            checkAuth(UI.getCurrent(), VaadinRequest.getCurrent(), 50, layout, notLoggedIn = {
+                val hasUsers = dbClient.transaction { UserTable.query { selectAll().toList() }.toList().isNotEmpty() }
+                if (hasUsers)
+                    noAccess()
+                else {
+                    verticalLayout {
+                        h2("There are no registered users just yet.\nPlease create an admin user now.")
+                        val nameField = textField("Name")
+                        val discordIDField = textField("Discord-ID")
+                        button("Create") {
+                            onClick {
+                                if (nameField.value.isNullOrBlank()) {
+                                    topNotification("Name is required.")
+                                    return@onClick
+                                }
+                                if (discordIDField.value.isNullOrBlank()) {
+                                    topNotification("A discord ID is required.")
+                                    return@onClick
+                                }
+                                dbClient.transaction {
+                                    UserTable.upsertItem(
+                                        User(
+                                            newGUID(),
+                                            nameField.value,
+                                            discordIDField.value,
+                                            System.currentTimeMillis(),
+                                            0L,
+                                            99
+                                        )
+                                    )
+                                }
+                                UI.getCurrent().page.reload()
+                            }
+                        }
+                    }
+                }
+            }) {
                 h2("Welcome, ${it.name}!")
                 init(initAdminView(it.permissions))
             }
